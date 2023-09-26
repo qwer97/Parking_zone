2896ed5ce5c6c7a4341e81077c98d717399f2a02
qwer97 <mcrohmh@naver.com>
2023-09-26 16:47:39 +0900

    api practice

--- src/main/java/Industryacademic/project/backend/Controller/BackendApplication.java
+++ src/main/java/Industryacademic/project/backend/Controller/BackendApplication.java
@@ -2,11 +2,15 @@ package Industryacademic.project.backend.Controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"industryacademic.project.backend.Service"})
+
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
---
+++ src/main/java/Industryacademic/project/backend/Service/firstService.java
@@ -0,0 +1,9 @@
+package Industryacademic.project.backend.Service;
+
+
+import org.springframework.context.annotation.Bean;
+
+
+public interface firstService {
+    String getFirstData();
+}
