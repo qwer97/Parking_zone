3fe7513cdae5cd95d31ce470fef51ef69d43c6ef
qwer97 <mcrohmh@naver.com>
2023-11-04 16:53:27 +0900

    카카오맵을 통한 주변 주차장 정보 받아오기

---
+++ src/main/java/Industryacademic/project/backend/Service/NearLotService.java
@@ -0,0 +1,41 @@
+package Industryacademic.project.backend.Service;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NearLotService {
+    @Value("${kakao.maps.api.key}")
+    private String kakaoMapsApiKey;
+    //이거 두개 다 application.properties 에 추가해야함
+    @Value("${kakao.maps.api.ka-header}")
+    private String kakaoMapsKaHeader;
+
+    private final RestTemplate restTemplate;
+
+    public NearLotService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String getNearbyParkingLots(double latitude, double longitude) {
+        //디폴트 값임. =주차장을 =음식점 으로 하면 음식점으로 바뀜
+        String apiUrl = "https://dapi.kakao.com/v2/local/search/keyword.json?query=주차장&category_group_code=PK6&size=10&x=" + longitude + "&y=" + latitude;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "KakaoAK " + kakaoMapsApiKey);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("KA", kakaoMapsKaHeader);
+
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.GET, entity, String.class);
+
+        return response.getBody();
+    }
+}
---
+++ src/test/java/Industryacademic/project/backend/Service/NearLotTest.java
@@ -0,0 +1,49 @@
+package Industryacademic.project.backend.Service;
+
+import Industryacademic.project.backend.BackendApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest(classes = BackendApplication.class)
+public class NearLotTest {
+
+    @Autowired
+    private NearLotService ns;
+
+    @MockBean
+    private RestTemplate restTemplate; // Mock RestTemplate 주입
+
+    @Test
+    public void testnear() {
+        double latitude = 37.12345;
+        double longitude = 127.67890;
+
+        // Mock API 응답 데이터
+        String mockApiResponse = "{\"result\": \"mocked_data\"}";
+
+        // Mock API 호출 및 응답 설정
+        when(restTemplate.exchange(anyString(), eq(HttpMethod.GET), any(HttpEntity.class), eq(String.class)))
+                .thenReturn(new ResponseEntity<>(mockApiResponse, HttpStatus.OK));
+
+        // getNearbyParkingLots 메서드 호출
+        String result = ns.getNearbyParkingLots(latitude, longitude);
+
+        // API 호출 확인
+        verify(restTemplate, times(1)).exchange(anyString(), eq(HttpMethod.GET), any(HttpEntity.class), eq(String.class));
+
+        // 결과 검증
+        assertEquals(mockApiResponse, result);
+    }
+}
